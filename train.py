6bbd7326873447a94e37a1e9934bb8e42b717c68
Alyx <callmegreninja@gmail.com>
2025-11-20 18:23:38 -0800

    Tuning hyperparameters

--- train.py
+++ train.py
@@ -8,9 +8,9 @@ from dataset import MonkeyDataset
 from utils import collate_fn
 from tqdm import tqdm
 
-NUM_EPOCHS = 30
-BATCH_SIZE = 8
-LEARNING_RATE = 1e-5
+NUM_EPOCHS = 5
+BATCH_SIZE = 16
+LEARNING_RATE = 1e-4
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
